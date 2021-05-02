3ee4989313f2aa3dc7434054f2d446165fdb95b2
Amrita <amrita.chanda@gmail.com>
2021-05-02 21:22:33 +0530

    Feature: Added 11ty lazy image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
+const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
+
+  eleventyConfig.addPlugin(lazyImagesPlugin, {
+    imgSelector: "img", // custom image selector
+    cacheFile: "", // don't cache results to a file
+  });
 
   return {
     dir: {
