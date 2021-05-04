93fb907c4bad0928c65fade0ef3dd1e96977d125
Amrita <amrita.chanda@gmail.com>
2021-05-04 21:46:16 +0530

    Debug: Images not showing up in VanillaSlider on homepage

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/utils");
 
   eleventyConfig.addPlugin(lazyImagesPlugin, {
     imgSelector: "img", // custom image selector
